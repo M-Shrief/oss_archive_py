3cb460b525666e380ff21a313bb39e977239cade
m-shrief <m.shrief.dev@proton.me>
2025-11-18 00:19:19 +0200

    Added mirror_oss() to start the mirroring process using Forgejo.

--- oss_archive/seeders/forgejo.py
+++ oss_archive/seeders/forgejo.py
@@ -6,6 +6,8 @@ from oss_archive.utils.logger import logger
 from oss_archive.components.forgejo.shared import base_headers
 from oss_archive.components.forgejo.schema import MigrateRepoReqBody
 
+DEFAULT_MIRROR_INTERVAL = "168h0m0s"
+
 async def create_org_for_mirrors() -> bool:
     try:
         admin_username = forgejo_config.get("admin_username")
@@ -54,3 +56,31 @@ async def is_oss_mirrored(oss_fullname: str):
         logger.error("Unknown error checking if OSS is mirrored", oss_fullname=oss_fullname, error=e)
         return False
 
+async def mirror_oss(oss_data: MigrateRepoReqBody):
+    try:
+        # Calling our API's which starts a task, and doesn't wait for Forgejo's response.
+        mirror_response = await httpx.async_post(
+            base_url=api_config.get("base_url") or "",
+            endpoint="/forgejo/repos/migrate",
+            body=oss_data.model_dump(mode="json"),
+            headers=base_headers,
+            timeout=Timeout(timeout=None, connect=10.0)
+            )
+
+        # Calling Forgejo's instance directly, and wait for the response.
+        # mirror_response = await httpx.async_post(
+        #     base_url=forgejo_config.get("base_url") or "",
+        #     endpoint="/repos/migrate",
+        #     body=oss_data.model_dump(mode="json"),
+        #     headers=base_headers,
+        #     timeout=Timeout(timeout=None, connect=10.0)
+        #     )
+
+        if mirror_response is None or mirror_response.status_code != 201:
+            return False
+        else:
+            return True
+        
+    except Exception as e:
+        logger.error("Unknown error mirroring OSS", oss_data=oss_data, error=e)
+        return False
