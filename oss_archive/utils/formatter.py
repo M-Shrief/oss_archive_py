0da8fe5eb2d2b133ec6457f15256414467d66f79
m-shrief <m.shrief.dev@proton.me>
2025-11-17 23:24:57 +0200

    Update OSS's fullname format to use '___' instead of ':' to achieve Forgejo's rules for repo names.

--- oss_archive/utils/formatter.py
+++ oss_archive/utils/formatter.py
@@ -1,7 +1,7 @@
 
 
 def format_oss_fullname(owner_username: str, repo_name: str)->str:
-    return f"{owner_username}:{repo_name}"
+    return f"{owner_username}___{repo_name}"
 
 def format_file_name(name_prefix: str, priority:int, file_number: int):
     return f"{name_prefix}-p{priority}-{file_number}"
