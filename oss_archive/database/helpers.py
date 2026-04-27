3e4ab81ca8637844015049c8607834e7a06c36e2
m-shrief <m.shrief.dev@proton.me>
2026-04-27 12:34:22 +0300

    Updated database's helper functions so that we can use params to automatically limit or add joined data for each entity: category, owner and OSS.

--- oss_archive/database/helpers.py
+++ oss_archive/database/helpers.py
@@ -1,6 +1,8 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from sqlalchemy import select, exc 
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+from pydantic import BaseModel, Field
 ### 
 from oss_archive.utils.logger import logger
 from oss_archive.database.models import  Category as CategoryModel, Owner as OwnerModel, OSS as OSSModel
@@ -68,18 +70,29 @@ async def does_oss_exists(oss_fullname: str, async_db: AsyncSession | None = Non
         logger.error("Error checking OSS existence", oss_fullname=oss_fullname, error=e)
         return None
 
+class DBQueryLimits(BaseModel):
+    limit: Annotated[int, Field(default=10, gt=0, le=100)]
+    offset: Annotated[int, Field(default=0, ge=0)]
 
-async def get_all_categories(async_db: AsyncSession | None = None, sync_db: Session | None = None):
+async def get_all_categories(with_owners: bool = False, with_oss: bool = False, query_limits: DBQueryLimits | None = None, async_db: AsyncSession | None = None, sync_db: Session | None = None):
     """Note: You have to pass async_db or sync_db, if you didn't it'll return None"""
     try:
         stmt = select(CategoryModel).order_by(CategoryModel.priority)
+        if with_owners:
+            stmt = stmt.options(joinedload(CategoryModel.owners).load_only(OwnerModel.id, OwnerModel.username, OwnerModel.priority, OwnerModel.reviewed))
+        if with_oss:
+            stmt = stmt.options(joinedload(CategoryModel.main_oss).load_only(OSSModel.id, OSSModel.fullname, OSSModel.priority, OSSModel.is_mirrored))
+
+        if query_limits is not None:
+            stmt = stmt.offset(query_limits.offset).limit(query_limits.limit)
+        
         if async_db is not None:
             res = await async_db.scalars(statement=stmt)
-            categories = res.all()
+            categories = res.unique().all()
             return categories
         elif sync_db is not None:
             res = sync_db.scalars(statement=stmt)
-            categories = res.all()
+            categories = res.unique().all()
             return categories
         else:
             return None
@@ -88,17 +101,25 @@ async def get_all_categories(async_db: AsyncSession | None = None, sync_db: Sess
         return None
 
 
-async def get_all_owners(async_db: AsyncSession | None = None, sync_db: Session | None = None):
+async def get_all_owners(category_key: str | None = None, with_oss: bool = False, query_limits: DBQueryLimits | None = None, async_db: AsyncSession | None = None, sync_db: Session | None = None):
     """Note: You have to pass async_db or sync_db, if you didn't it'll return None"""
     try:
         stmt = select(OwnerModel).order_by(OwnerModel.priority)
+        if category_key is not None:
+            stmt = stmt.where(OwnerModel.main_category_key == category_key)
+        if with_oss:
+            stmt = stmt.options(joinedload(OwnerModel.owned_oss).load_only(OSSModel.id, OSSModel.fullname, OSSModel.priority, OSSModel.is_mirrored))
+        
+        if query_limits is not None:
+            stmt = stmt.offset(query_limits.offset).limit(query_limits.limit)
+        
         if async_db is not None:
             res = await async_db.scalars(statement=stmt)
-            owners = res.all()
+            owners = res.unique().all()
             return owners
         elif sync_db is not None:
             res = sync_db.scalars(statement=stmt)
-            owners = res.all()
+            owners = res.unique().all()
             return owners
         else:
             return None
@@ -106,17 +127,26 @@ async def get_all_owners(async_db: AsyncSession | None = None, sync_db: Session 
         logger.error("Unknown error getting all owners", error=e)
         return None
 
-async def get_all_oss(async_db: AsyncSession | None = None, sync_db: Session | None = None):
+async def get_all_oss(category_key: str | None = None, owner_username: str | None = None, query_limits: DBQueryLimits | None = None, async_db: AsyncSession | None = None, sync_db: Session | None = None):
     """Note: You have to pass async_db or sync_db, if you didn't it'll return None"""
     try:
         stmt = select(OSSModel).order_by(OSSModel.priority)
+        if owner_username is not None: # if owner is specified, then we don't need to care about main_category_key
+            stmt = stmt.where(OSSModel.owner_username == owner_username)
+        if category_key is not None:
+            stmt = stmt.where(OSSModel.main_category_key == category_key)
+        
+        if query_limits is not None:
+            stmt = stmt.offset(query_limits.offset).limit(query_limits.limit)
+        
+
         if async_db is not None:
             res = await async_db.scalars(statement=stmt)
-            oss_list = res.all()
+            oss_list = res.unique().all()
             return oss_list
         elif sync_db is not None:
             res = sync_db.scalars(statement=stmt)
-            oss_list = res.all()
+            oss_list = res.unique().all()
             return oss_list
         else:
             return None
