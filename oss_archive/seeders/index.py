da01032640507d478dec983b4007fff8bef7d8ff
m-shrief <m.shrief.dev@proton.me>
2026-04-27 16:52:01 +0300

    modularized mirror_all_oss to use another function called mirror_oss_list(), and by doing that we impproved the reusability of the function to use them elsewhere.

--- oss_archive/seeders/index.py
+++ oss_archive/seeders/index.py
@@ -8,8 +8,7 @@ from oss_archive.database.models import Category as CategoryModel, Owner as Owne
 from oss_archive.database import helpers as db_helpers
 from oss_archive.seeders.json import seed_all as seed_all_json
 from oss_archive.seeders.sources import github as github_source, codeberg as codeberg_source
-from oss_archive.seeders.forgejo import create_org_for_mirrors, is_oss_mirrored, mirror_oss, DEFAULT_MIRROR_INTERVAL
-from oss_archive.components.forgejo.schema import MigrateRepoReqBody, ServiceEnum
+from oss_archive.seeders.forgejo import create_org_for_mirrors, create_migrate_repo_req_body, is_oss_mirrored, mirror_oss, DEFAULT_MIRROR_INTERVAL
 
 async def initialize_all_seed_ops(db: Session):
     ### Make requests to outer APIs async, but the seeding operation can by sync
@@ -62,43 +61,53 @@ async def seed_owner_oss_from_source(owner: OwnerModel, db: Session): #-> Owner:
             return None
 
 
+
 async def mirror_all_oss(db: Session):
-    oss_list = await db_helpers.get_all_oss(sync_db=db)
-    if oss_list is None:
-        return
+    try:
+        org_for_mirrors = forgejo_config.get("org_for_mirrors")
+        if org_for_mirrors is None:
+            return False
 
-    if ENV == "dev": # if we're in development limit them to 10 OSS only.
-        oss_list = oss_list[:10]
+        oss_list = await db_helpers.get_all_oss(sync_db=db)
+        if oss_list is None:
+            return False
 
-    org_for_mirrors = forgejo_config.get("org_for_mirrors")
-    if org_for_mirrors is None:
-        return
+        if ENV == "dev": # if we're in development limit them to 3 only.
+            oss_list = oss_list[:3]
 
-    for oss in oss_list:
-        if oss.clone_url is None:
-            logger.info("Failed to mirror OSS, as it's clone_url doesn't exists", oss_fullname=oss.fullname)
-            continue
+        _ = await mirror_oss_list(oss_list)
 
+        return True
+    except Exception:
+        logger.error("Couldn't seed Owners' JSON files")
+        return False
 
-        is_mirrored = await is_oss_mirrored(oss.fullname)
-        if is_mirrored is True:
-            logger.info("OSS is already mirrored.", oss_fullname=oss.fullname)
-            continue
+async def mirror_oss_list(oss_list: Sequence[OSSModel]):
+    try:
+        org_for_mirrors = forgejo_config.get("org_for_mirrors")
+        if org_for_mirrors is None:
+            return False
 
+        for oss in oss_list:
+            is_mirrored = await is_oss_mirrored(oss.fullname)
+            if is_mirrored is True:
+                logger.info("OSS is already mirrored.", oss_fullname=oss.fullname)
+                continue
 
-        oss_data = MigrateRepoReqBody(
-            clone_addr=oss.clone_url,
-            repo_name=oss.fullname,
-            repo_owner=org_for_mirrors,
-            service=ServiceEnum.git,
-            mirror=True,
-            mirror_interval=DEFAULT_MIRROR_INTERVAL
-        )
-        did_mirror_oss = await mirror_oss(oss_data)
+            oss_data = create_migrate_repo_req_body(oss, org_for_mirrors)
+            if oss_data is None:
+                continue
 
-        if did_mirror_oss is True:
-            logger.info("Started the Mirroring procces for the OSS successfully.", oss_fullname=oss.fullname)
-        else:
-            logger.error("Failed to start the Mirroring procces for the OSS.", oss_fullname=oss.fullname)
-    
-        sleep(2.0)
+            did_mirror_oss = await mirror_oss(oss_data)
+
+            if did_mirror_oss is True:
+                logger.info("Started the Mirroring procces for the OSS successfully.", oss_fullname=oss.fullname)
+            else:
+                logger.error("Failed to start the Mirroring procces for the OSS.", oss_fullname=oss.fullname)
+        
+            sleep(2.0)
+
+        return True
+    except Exception:
+        logger.error("Couldn't seed Owners' JSON files")
+        return False
